1b47d03032ee4087737de264271725e437f70100
Aryanjangde <aryan.jangde821@gmail.com>
2024-07-21 22:27:11 +0530

    updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}api/:path*` // Use environment variable for base URL
+      }
+    ];
+  }
 };
 
 export default nextConfig;
